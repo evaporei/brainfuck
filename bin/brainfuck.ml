cf3fbfb81a14c7e4b1f821b9778863147787a4d9
Otávio Pace <otaviopp8@gmail.com>
2021-05-22 16:50:05 -0300

    bin: Add file reading

--- bin/brainfuck.ml
+++ bin/brainfuck.ml
@@ -1,1 +1,13 @@
-let () = print_endline "Hello, World"
+let read_file file_name =
+  let file_channel = open_in file_name in
+    try
+      let content = really_input_string file_channel (in_channel_length file_channel) in
+      close_in file_channel;
+      content
+    with e ->
+      close_in_noerr file_channel;
+      raise e
+
+let () =
+  let source_code = read_file "samples/helloworld.bf" in
+  print_endline source_code
