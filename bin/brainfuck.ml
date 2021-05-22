30b588460333bd635ab5b6d3d8e9eda7c62aa48f
Otávio Pace <otaviopp8@gmail.com>
2021-05-22 17:32:07 -0300

    bin: Make file name be read as CLI arg

--- bin/brainfuck.ml
+++ bin/brainfuck.ml
@@ -1,3 +1,12 @@
+exception Err of string
+
+let setup_err_printer =
+  Printexc.register_printer
+    (function
+      | Err s -> Some (Printf.sprintf "%s" s)
+      | _ -> None
+    )
+
 let read_file file_name =
   let file_channel = open_in file_name in
     try
@@ -8,6 +17,24 @@ let read_file file_name =
       close_in_noerr file_channel;
       raise e
 
+let parse_arg n =
+  (Sys.argv
+  |> Array.to_list
+  |> List.nth_opt)
+  n
+
+let get_file_name =
+  match parse_arg 1 with
+    | Some arg -> arg
+    | None -> raise (Err "Missing source file name, pass it as first command line argument")
+
 let () =
-  let source_code = read_file "samples/helloworld.bf" in
-  print_endline source_code
+  setup_err_printer;
+  try
+    get_file_name
+    |> read_file
+    |> print_endline
+  with e ->
+    let err_msg = Printexc.to_string e in
+    print_endline @@ "Fatal error: " ^ err_msg;
+    exit 1
